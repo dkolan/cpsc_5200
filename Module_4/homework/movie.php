4096896156766b8a88dda0e4543ceb84a3fe8992
Dan Kolan <dan.kolan@gmail.com>
2023-06-04 22:09:47 -0500

    improved formatting, tabs, linebreaks, etc.

--- Module_4/homework/movie.php
+++ Module_4/homework/movie.php
@@ -13,15 +13,14 @@ that corresponds to that film.
 <html>
 	<!-- Get the film info from the query param's corresponding info.txt -->
 	<?php
-		$filmQueryParam = $_GET["film"];
-		$filmInfoList = getFilmInfo($filmQueryParam);			
-		$filmName = trim($filmInfoList[0]);
-		$filmYear = trim($filmInfoList[1]);
-		$filmScore = trim($filmInfoList[2]);
+	$filmQueryParam = $_GET["film"];
+	$filmInfoList = getFilmInfo($filmQueryParam);			
+	$filmName = trim($filmInfoList[0]);
+	$filmYear = trim($filmInfoList[1]);
+	$filmScore = trim($filmInfoList[2]);
 	?>
 	<head>
-		<title><?= $filmName ?> - Rancid Tomatoes</title>
-
+		<title>Rancid Tomatoes</title>
 		<meta charset="utf-8" />
 		<link href="movie.css" type="text/css" rel="stylesheet" />
 		<link rel="icon" type="image/x-icon" href="rotten.gif">
@@ -30,96 +29,81 @@ that corresponds to that film.
 	<body>
 		<div id="banner-top">
 			<img src="banner.png" alt="Rancid Tomatoes" />
-		</div>
-	
-		<h1 class="movie-title"><?= $filmName ?> (<?= $filmYear?>) </h1>
-		
+		</div>	
+		<h1 class="movie-title"><?= $filmName ?> (<?= $filmYear?>) </h1>		
 		<div class="overall-content-container">
 			<div id="overview-container">
 				<div>
 					<!-- Assign image based on filmQueryParam -->
 					<img id="overview-img" src=<?= "$filmQueryParam/overview.png" ?> alt="general overview" />
 				</div>
-
 				<!-- General Overview info, build a key:value map of term:definition. -->
 				<?php
-					$generalOverviewInfoMap = buildFilmGeneralOverviewMap($filmQueryParam);
+				$generalOverviewInfoMap = buildFilmGeneralOverviewMap($filmQueryParam);
 				?>
 				<dl>
 				<!-- Iterate over general overview data and render terms/definitions -->
 				<?php
-					foreach ($generalOverviewInfoMap as $infoKey => $infoVal) {
+				foreach ($generalOverviewInfoMap as $infoKey => $infoVal) {
 				?>
-						<dt><?= $infoKey ?></dt>
-						<dd><?= $generalOverviewInfoMap[$infoKey] ?></dd>
+					<dt><?= $infoKey ?></dt>
+					<dd><?= $generalOverviewInfoMap[$infoKey] ?></dd>
 				<?php
-					}
+				}
 				?>
 				</dl>
 			</div>
-
 			<div class="score-review-container">
 				<div class="score-container">
 					<!-- Assign the score logo depending on whether the score is Rotten (<60) or Fresh (>= 60) -->
 					<img id="rotten-logo" src=<?= ($filmScore < 60) ? "rottenbig.png" : "freshbig.png" ?> alt="Rotten" />
 					<span id="avg-rating"><?= $filmScore ?>%</span>
-				</div>
-				
+				</div>			
 				<?php					
-					// Get list of review files
-					$reviewFileList = glob($filmQueryParam . "/review*");
+				// Get list of review files
+				$reviewFileList = glob($filmQueryParam . "/review*");
 
-					// Calculate # of reviews in left column
-					$leftColReviewCount = ceil(count($reviewFileList)/2);
-					error_log(print_r("!!!!!!!!!!!!!!! DEBUG !!!!!!!!!!!!!!!\nleftColReviewCount=".$leftColReviewCount, TRUE)); 
-					error_log(print_r("!!!!!!!!!!!!!!! DEBUG !!!!!!!!!!!!!!!\count(reviewFileList)=".count($reviewFileList), TRUE)); 
-
-					?>
-					<div class="review-container">
-						<div class="review-column-left">
-					<?php
-
-					// Loop through review file list
-					// If $i > $leftColReviewCount, belongs in right col, else left
-					for($i = 0; $i < count($reviewFileList); $i++) {
-
-						// Reviews have exactly 4 lines: review, fresh/rotten, name, publication
-						$reviewFileLines = file($reviewFileList[$i]);
-						$reviewText = trim($reviewFileLines[0]);
-						$freshOrRotten = trim($reviewFileLines[1]);
-						$criticName = trim($reviewFileLines[2]);
-						$criticPublication = trim($reviewFileLines[3]);
-						?>
-
-							<div class="review">
-								<p class="review-paragraph">
-									<!-- Set review fresh/rotten image and review text, critic, and publication -->
-									<img src=<?= (strcasecmp($freshOrRotten, "FRESH") == 0) ? "fresh.gif" : "rotten.gif" ?> 
-										alt=<?= (strcasecmp($freshOrRotten, "FRESH") == 0) ? "Fresh" : "Rotten" ?> />  
-									<q><?= $reviewText ?></q>
-								</p>
-								<p class="review-author">
-									<img src="critic.gif" alt="Critic" />
-									<?= $criticName ?> <br />
-									<?= $criticPublication ?>
-								</p>
-							</div>
-						
-						<?php
-
-						// If left column amount reached, make next column div
-						if($i+1 == $leftColReviewCount) {
-							?>
-							</div>
-							<div class="review-column-right">
-							<?php
-						}
-					}
-					?>
-					</div>
-					</div>
-					<?php
+				// Calculate # of reviews in left column
+				$leftColReviewCount = ceil(count($reviewFileList)/2); 
 				?>
+				<div class="review-container">
+					<div class="review-column-left">
+				<?php
+				// Loop through review file list
+				// If $i > $leftColReviewCount, belongs in right col, else left
+				for($i = 0; $i < count($reviewFileList); $i++) {
+					// Reviews have exactly 4 lines: review, fresh/rotten, name, publication
+					$reviewFileLines = file($reviewFileList[$i]);
+					$reviewText = trim($reviewFileLines[0]);
+					$freshOrRotten = trim($reviewFileLines[1]);
+					$criticName = trim($reviewFileLines[2]);
+					$criticPublication = trim($reviewFileLines[3]);
+					?>
+					<div class="review">
+						<p class="review-paragraph">
+							<!-- Set review fresh/rotten image and review text, critic, and publication -->
+							<img src=<?= (strcasecmp($freshOrRotten, "FRESH") == 0) ? "fresh.gif" : "rotten.gif" ?> 
+								alt=<?= (strcasecmp($freshOrRotten, "FRESH") == 0) ? "Fresh" : "Rotten" ?> />  
+							<q><?= $reviewText ?></q>
+						</p>
+						<p class="review-author">
+							<img src="critic.gif" alt="Critic" />
+							<?= $criticName ?> <br />
+							<?= $criticPublication ?>
+						</p>
+					</div>						
+					<?php
+					// If left column amount reached, make next column div
+					if($i+1 == $leftColReviewCount) {
+					?>
+						</div>
+						<div class="review-column-right">
+						<?php
+					}
+				}
+				?>
+					</div>
+				</div>
 			</div>
 			<div class="page-count-container">
 				<p class="page-count-text">(1-10) of 88</p>
