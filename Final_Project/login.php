b929d3cd71b5e49e87b2c0757213f26be5dd8bb1
Dan Kolan <dan.kolan@gmail.com>
2023-06-14 23:51:29 -0500

    login form and styles, need to submit data

---
+++ Final_Project/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/login.css" type="text/css" rel="stylesheet">
+    <title>Setlist Manager - Log In</title>
+</head>
+<body>
+    <div class="login-container">
+        <h2>Setlist Tracker</h2>
+        <form>
+            <input class="text-input" type="text" placeholder="Username" required="true">
+            <input class="text-input" type="password" placeholder="Password" required="true">
+            <div class="remember-checkbox"><label><input type="checkbox" /> Remember me</label></div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</body>
+</html>
